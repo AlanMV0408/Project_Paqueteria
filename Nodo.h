00f223ad35b03552f90d49f75fc1d04617ce9b32
AlanMV <mv.alan4806@gmail.com>
2026-05-14 11:47:54 -0600

    [Add]: Nodo class & Arista class

--- Nodo.h
+++ Nodo.h
@@ -1,7 +1,47 @@
 #ifndef NODO_H
 #define NODO_H
 
-// Contenido del archivo
+#include "Sucursal.h"
+
+class Nodo; // declaración adelantada para hacer uso de ella
+
+class Arista
+{
+private:
+    Nodo *destino;
+    int peso;
+    Arista *sig;
+
+public:
+    Arista(Nodo *destino, int peso); //parametros principales
+    Nodo *getDestino();     //devuelve nodo destino de la arista
+    int getPeso();      //devuelve  peso de la arista
+    Arista *getSig();   //devuelve la siguiente arista en la lista de adyacencia
+    void setSig(Arista *sig); //marca la siguiente arista en la lista
+};
+
+class Nodo
+{
+private:
+    Sucursal sucursal;
+    Nodo *sig;
+    Arista *listaAristas; // Lista de adyacencia para Rutas
+
+public:
+    //Constructor
+    Nodo(Sucursal sucursal); 
+
+    //Getters 
+    Sucursal getSucursal();
+    Nodo *getSig();
+    Arista *getListaAristas();
+
+    //Setters
+    void setSig(Nodo *sig);
+    
+    //Funciones de Aristas
+    void agregarArista(Nodo *destino, int peso);
+    void eliminarArista(string nombreDestino);
+};
 
 #endif // NODO_H
-
