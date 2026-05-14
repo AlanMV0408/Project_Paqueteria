7baafd29f74d6e6a0066ebf662cd19e6301689e9
AlanMV <mv.alan4806@gmail.com>
2026-05-14 12:27:05 -0600

    [Fix]: Add "constructor" was in sucursal.h but no in sucursal.cpp

--- Sucursal.cpp
+++ Sucursal.cpp
@@ -1,20 +1,20 @@
 #include "Sucursal.h"
 #include <iostream>
 
+Sucursal::Sucursal() : nombre(""), direccion(""), telefono(""), horario(""){} //Constructor por defecto que inicializa los atributos con valores vacíos
 
-
-Sucursal::Sucursal(const string& Nombre,const string& Direccion,const string& Telefono,const string& Horario){
-
+Sucursal::Sucursal(const string &Nombre, const string &Direccion, const string &Telefono, const string &Horario) //Constructor que recibe los valores de los atributos y los asigna a las variables miembro
+{
     nombre = Nombre;
     direccion = Direccion;
     telefono = Telefono;
     horario = Horario;
 }
 
-void Sucursal::imprimir() const {
-    std::cout << "  Nombre    : " << nombre    << "\n"
+void Sucursal::imprimir() const //imprime los detalles de la sucursal
+{
+    std::cout << "  Nombre    : " << nombre << "\n"
               << "  Dirección : " << direccion << "\n"
-              << "  Teléfono  : " << telefono  << "\n"
-              << "  Horario   : " << horario   << "\n";
+              << "  Teléfono  : " << telefono << "\n"
+              << "  Horario   : " << horario << "\n";
 }
- 
