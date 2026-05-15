c1c73879584a9158f9ddb948b0f35993d5256277
JOKE06e <jh2650549@gmail.comm>
2026-05-14 20:16:59 -0600

    Agregue el .cpp y .h del Grafo

--- Grafo.cpp
+++ Grafo.cpp
@@ -1,2 +1,119 @@
 #include "Grafo.h"
 #include "Nodo.h"
+#include <iostream>
+
+
+Grafo::Grafo(){
+
+    inicioNodo = nullptr;
+}
+
+void Grafo::agregarSucursal(Sucursal sucursal)
+{
+    Nodo *nuevo = new Nodo(sucursal); //crea una sucursal nueva en un vertice 
+
+    if(inicioNodo == nullptr)
+    {
+        inicioNodo = nuevo;
+    }
+    else
+    {
+        Nodo *aux = inicioNodo;
+
+        while(aux->getSig() != nullptr)
+        {
+            aux = aux->getSig();
+        }
+
+        aux->setSig(nuevo);
+    }
+
+    
+}
+
+
+Nodo *Grafo::BuscarSucursal(string nombre) //busca la sucursal por su nombre 
+{
+    Nodo *aux = inicioNodo; // 
+
+    while(aux != nullptr)
+    {
+        if(aux->getSucursal().getNombre() == nombre)
+        {
+            return aux;
+        }
+
+        aux = aux->getSig();
+    }
+
+    return nullptr;
+}
+
+
+void Grafo::conexion(string origen, string destino, int peso){
+
+    Nodo *nodoOrigen = BuscarSucursal(origen);
+    Nodo *nodoDestino = BuscarSucursal(destino);
+
+    if(nodoOrigen == nullptr  &&  nodoDestino == nullptr){
+
+        cout<<"Sucursal no existe en este momento."<<endl; 
+        return; 
+    }
+
+
+    nodoOrigen->agregarArista(nodoDestino, peso);  //conecta el origen con el destino
+
+    nodoDestino->agregarArista(nodoOrigen, peso);  //e lo contrario, conecta el destino con el origen
+
+
+}
+
+
+
+void Grafo::eliminarConexion(string origen, string destino){
+    Nodo *nodoOrigen = BuscarSucursal(origen);
+    Nodo *nodoDestino = BuscarSucursal(destino);
+
+    if(nodoOrigen == nullptr ||
+       nodoDestino == nullptr)
+    {
+        cout << "Sucursal no encontrada." << endl;
+        return;
+    }
+
+    nodoOrigen->eliminarArista(destino);
+    nodoDestino->eliminarArista(origen);
+
+}
+
+void Grafo::mostrarGrafo()
+{
+    Nodo *aux = inicioNodo;
+
+    while(aux != nullptr)
+    {
+        cout << "\nSucursal: "
+             << aux->getSucursal().getNombre()
+             << endl;
+
+        Arista *auxArista =
+            aux->getListaAristas();
+
+        while(auxArista != nullptr)
+        {
+            cout << " -> "
+                 << auxArista->getDestino()
+                    ->getSucursal()
+                    .getNombre()
+
+                 << " Peso: "
+                 << auxArista->getPeso()
+                 << endl;
+
+            auxArista = auxArista->getSig();
+        }
+
+        aux = aux->getSig();
+    }
+}
--- Grafo.h
+++ Grafo.h
@@ -1,7 +1,35 @@
 #ifndef GRAFO_H
 #define GRAFO_H
+#include "Nodo.h"
 
-// Contenido del archivo
+using namespace std;
+
+class Grafo{
+
+private:
+
+    Nodo* inicioNodo; //Es el h del nodo vertices
+
+    public:
+
+     Grafo();
+
+     void agregarSucursal(Sucursal Sucursal);
+     Nodo* BuscarSucursal(string nombre);
+
+     void EliminarSucursal(string nombre);
+
+     void conexion(string origen, string destino,int  peso);
+
+     void eliminarConexion(string origen,string destino);
+
+    // Mostrar
+    void mostrarGrafo();
+     
+    ~Grafo();
+
+};  
+
 
 #endif // GRAFO_H
 
