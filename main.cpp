1163a6c61ec37a6b6c94d8da99779feac18d6e86
AlanMV <mv.alan4806@gmail.com>
2026-05-17 16:27:07 -0600

    [Add]: menu structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,53 @@
 #include "Grafo.h"
 #include "Nodo.h"
+#include "Sucursal.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    cout<<"Hello World"<<endl;  
-    return 0;
+    
+
+    int opcion;
+    
+    do{
+        cout"------- Distribucion Paqueteria -------"<<endl;
+        cout<<"1) Insertar Sucursal(vertice/nodo)"<<endl;
+        cout<<"2) Insertar Arista(conexion)"<<endl;
+        cout<<"3) Eliminar Vertice"<<endl;
+        cout<<"4) Eliminar Arista"<<endl;
+        cout<<"5) Mostrar listado de vertices/aristas"<<endl;
+        cout<<"6) Recorrido Minimo (Dijkstra) entre 2 sucursales"<<endl;
+        cout<<"7) Salir"<<endl;
+        cout<<"Ingrese una opcion: ";
+        cin>>opcion;
+
+        if(opcion == 1){
+
+        }
+        else if(opcion == 2){
+
+        }
+        else if(opcion == 3){
+
+        }
+        else if(opcion == 4){
+
+        }
+        else if(opcion == 5){
+
+        }
+        else if(opcion == 6){
+
+        }
+        else if(opcion == 7){
+            cout<<"Hasta Pronto."<<endl;
+            break;
+        }
+        else{
+            cout<<"Opcion no valida. Intente de nuevo."<<endl;
+        }   
+    }while(opcion != 8 && opcion != 0);
+
 }
 
